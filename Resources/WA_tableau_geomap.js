82fce2f13e1f8547922a41a9fd5446a1b33467c3
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-03-11 20:23:05 -0700

    file commit

--- Resources/WA_tableau_geomap.js
+++ Resources/WA_tableau_geomap.js
@@ -1,9 +1,9 @@
 //Tableau Embed function
 function initViz() {
-    url = "https://public.tableau.com/views/finalproject_16151831568920/2020GNIC?:language=en&:display_count=y&:origin=viz_share_link",
+    url = "https://public.tableau.com/shared/KDQRZ44W7?:display_count=y&:origin=viz_share_link",
     options = {
         hideToolbar: true,
-        width: "600px",
+        width: "800px",
         height: "600px",
     };
     viz = new tableau.Viz(geomap, url, options);
@@ -11,7 +11,7 @@ function initViz() {
     url2= "https://public.tableau.com/views/Book1_16153169893270/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link",
     options = {
         hideToolbar: true,
-        width: "600px",
+        width: "800px",
         height: "600px",
     };
     viz2 = new tableau.Viz(radialbarchart, url2, options);
